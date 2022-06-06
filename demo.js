1e435b77c72eb811270ce2533e910b00845573b1
Kai-Shen Deru <kderu1420@gmail.com>
2022-06-06 11:44:17 -0600

    Add a framerate display

--- demo.js
+++ demo.js
@@ -1,3 +1,9 @@
 const edges = [[1, 2], [2, 3], [1, 4], [1, 3], [5, 6], [6, 7], [6, 9], [50, 99], [50, 51], [50, 67], [54, 64], [64, 67]];
 
 const graph = new Graph('demo', new Array(100).fill({}), edges);
+
+const framerateDisplay = document.getElementById('framerate');
+
+setInterval(() => {
+  framerateDisplay.innerText = graph.framerate;
+}, 500);
