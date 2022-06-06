5de2941a74ce867cff7b2138df30245a4f9ddb0a
Kai-Shen Deru <kderu1420@gmail.com>
2022-06-06 11:43:57 -0600

    Add more nodes and edges

--- demo.js
+++ demo.js
@@ -1,1 +1,3 @@
-const graph = new Graph('demo', new Array(10).fill({}), [[1, 2], [2, 3], [1, 4]]);
+const edges = [[1, 2], [2, 3], [1, 4], [1, 3], [5, 6], [6, 7], [6, 9], [50, 99], [50, 51], [50, 67], [54, 64], [64, 67]];
+
+const graph = new Graph('demo', new Array(100).fill({}), edges);
